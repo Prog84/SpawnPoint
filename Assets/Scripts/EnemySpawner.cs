50c19da869c49ab9d1f2c9ce5643e04f14a5e6b9
Prog84 <bvv198424@rambler.ru>
2020-09-28 16:41:07 +1000

    Initial Commit

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private Enemy _template;
+    [SerializeField] private Transform[] _spawnPoints;
+    private void Start()
+    {
+        StartCoroutine(SpawnEnemy());
+    }
+
+    private IEnumerator SpawnEnemy()
+    {
+        var numberEnemy = 0;
+        var waitTwoSeconds = new WaitForSeconds(2f);
+        while (true)
+        {
+            var enemyPosition = numberEnemy % 3;
+            var createdEnemy = Instantiate(_template, _spawnPoints[enemyPosition].position, Quaternion.identity);
+            numberEnemy++;
+            yield return waitTwoSeconds;
+        }
+    }
+}
